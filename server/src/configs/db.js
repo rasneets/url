3a22b80b89b12b375024bd209cb3c4cb614de33b
rasneets <128911878+rasneets@users.noreply.github.com>
2025-07-15 12:20:14 +0530

    Update db.js

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -4,7 +4,8 @@ require("dotenv").config();
 // const DB_URL = 'mongodb://localhost:27017/urlshortener';
 
 const connect = () => {
-    return mongoose.connect(process.env.DB_URL);
+    return mongoose.connect(process.env.DB_URL, {useNewUrlParser: true,
+  useUnifiedTopology: true,});
 };
 
 module.exports = connect;
